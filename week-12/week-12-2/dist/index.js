29e4ce66d9f992916f9c4d67f1d6ac32c4e3e60d
krixech <krixech12@gmail.com>
2024-04-20 18:34:21 +0530

    Week-12 is started

---
+++ week-12/week-12-2/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const displayUser = (user) => {
+    console.log(`Name: ${user.name} & Email: ${user.email}`);
+};
+displayUser({
+    name: 'krish',
+    email: '2ihoifho@gmail.com'
+});
